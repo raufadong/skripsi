1953bdfae77f93adf28fcb393366ab7656cb8de0
raufadong <119052794+raufadong@users.noreply.github.com>
2025-07-28 20:31:34 +0700

    appv7_final_with_distribusi.py

--- appv7_final_with_distribusi.py
+++ appv7_final_with_distribusi.py
@@ -103,12 +103,12 @@ if uploaded_file is not None:
                     label_mapping = {i: str(i) for i in np.unique(y)}
 
                 # Filter hanya kolom kategori yang tidak semuanya kosong
-valid_categorical_cols = [col for col in X.select_dtypes(include='object').columns if X[col].notna().any()]
-if valid_categorical_cols:
-    X[valid_categorical_cols] = SimpleImputer(strategy='most_frequent').fit_transform(X[valid_categorical_cols])
-X = impute_missing(X)
-    st.subheader("📌 Jumlah Nilai Kosong Setelah Imputasi")
-    st.write(pd.DataFrame(X).isnull().sum())
+                valid_categorical_cols = [col for col in X.select_dtypes(include='object').columns if X[col].notna().any()]
+                if valid_categorical_cols:
+                X[valid_categorical_cols] = SimpleImputer(strategy='most_frequent').fit_transform(X[valid_categorical_cols])
+                X = impute_missing(X)
+                st.subheader("📌 Jumlah Nilai Kosong Setelah Imputasi")
+                st.write(pd.DataFrame(X).isnull().sum())
 
                 balancing_method = st.radio("🔄 Pilih metode penanganan imbalance:", ["Tanpa Balancing", "SMOTE", "NearMiss"])
                 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
