611236cb4e8538802d3ee3754058596fbd82cf43
raufadong <119052794+raufadong@users.noreply.github.com>
2025-07-28 20:48:30 +0700

    appv7_final_with_distribusi.py

--- appv7_final_with_distribusi.py
+++ appv7_final_with_distribusi.py
@@ -105,7 +105,7 @@ if uploaded_file is not None:
                 # Filter hanya kolom kategori yang tidak semuanya kosong
 valid_categorical_cols = [col for col in X.select_dtypes(include='object').columns if X[col].notna().any()]
 if valid_categorical_cols:
-X = impute_missing(X)
+    X = impute_missing(X)
                 st.subheader("📌 Jumlah Nilai Kosong Setelah Imputasi")
                 st.write(pd.DataFrame(X).isnull().sum())
 
